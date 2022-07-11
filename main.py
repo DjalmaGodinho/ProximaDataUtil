1e915cd71bd2f6033c2e01f79310a7bd0f6ea0c2
Djalma Godinho <djalma56godinho@gmail.com>
2022-07-11 20:51:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,32 +9,35 @@ def nextDateIfSaturadyAndMonday(data: datetime):
         return data + timedelta(days=1)
     return data
 
-prazoDias = 360
-startDate = date.today()
-endDate = date.today() + timedelta(days=prazoDias)
+def main(event, prazoDias):
+    startDate = date.today()
+    endDate = date.today() + timedelta(days=prazoDias)
 
-feriadosNacionais = pd.read_excel("C:/Trabalho/feriados_nacionais.xls")
-datasFeriados = feriadosNacionais['Data']
-feriadosAcimaHoje = [data for data in datasFeriados if data >= startDate and data <= endDate]
+    feriadosNacionais = pd.read_excel("C:/Trabalho/feriados_nacionais.xls")
+    datasFeriados = feriadosNacionais['Data']
+    feriadosAcimaHoje = [data for data in datasFeriados if data >= startDate and data <= endDate]
 
-datas = []
-for i in range(prazoDias+1):
-    data = startDate + timedelta(days=i)
-    if data.weekday() not in (5,6): # retirda o sabado e o domingo da data
-        datas.append(data)
+    datas = []
+    for i in range(prazoDias+1):
+        data = startDate + timedelta(days=i)
+        if data.weekday() not in (5,6): # retirda o sabado e o domingo da data
+            datas.append(data)
 
-if feriadosAcimaHoje:
-    for feriado in feriadosAcimaHoje:        
-        if feriado == endDate:
-            if endDate.weekday() < 5:
-                endDate = endDate + timedelta(days=1)
-            else:
-                endDate = nextDateIfSaturadyAndMonday(endDate)
-            print(endDate.strftime('%d/%m/%Y'))
-            break    
+    if feriadosAcimaHoje:
+        for feriado in feriadosAcimaHoje:        
+            if feriado == endDate:
+                if endDate.weekday() < 5:
+                    endDate = endDate + timedelta(days=1)
+                else:
+                    endDate = nextDateIfSaturadyAndMonday(endDate)
+                print(endDate.strftime('%d/%m/%Y'))
+                break    
 
-if datas:
-    for data in datas:
-        if data == endDate:
-            print('Prox. data util: ', data)
-            break
+    if datas:
+        for data in datas:
+            if data == endDate:
+                endDate = data
+                print('Prox. data util: ', data)
+                break
+    
+    return endDate
